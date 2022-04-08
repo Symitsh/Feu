88e86891a2a9635ac0e7426387f1baaa5c8dc866
Symitsh <sandri822@hotmail.fr>
2022-04-08 18:37:59 +0200

    Création feu00.rb

---
+++ feu00.rb
@@ -0,0 +1,18 @@
+# ***** Echauffement *****
+# Méthodes utilisées
+
+
+
+
+
+# Partie 1: Gestion d'erreur
+(puts "error"; exit) if ARGV.size != 2
+
+# Partie 2 : Parsing
+
+
+# Partie 3: Résolution
+
+
+# Partie 4: Affichage
+
