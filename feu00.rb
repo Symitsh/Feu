2d73533bff5c11daa94e8ce68436653101053a57
Sandri Florent <sandri822@hotmail.fr>
2022-04-11 16:38:49 +0200

    Modifications de la méthode draw_rectangle

--- feu00.rb
+++ feu00.rb
@@ -1,45 +1,50 @@
 # ***** Echauffement *****
-# Méthodes utilisées
 class MyRectangle
   # Dessine un rectangle d'une largeur et d'une hauteur données
   def draw_rectangle(width, height)
-    # Boucle qui contrôle la hauteur du rectangle
     i = 0
     while (i < height)
-      # Boucle qui contrôle la largeur du rectangle
       j = 0
       while (j < width)
-        # Affiche la couche externe du rectangle
-        #print i, j
-        if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
-          print "* "
-        else
-          print "  "
+        # Affiche les extrémités du rectangle
+        if (i == (height - 1) && j == (width - 1) || i == (height - 1) && j == 0 ||
+          i == 0 && j == (width - 1) || i == 0 && j == 0)
+          print "o"
+        end
+        # Affiche le côté gauche et droit du rectangle
+        if j == 0 && (i!=0) && (i!=height-1)
+          print "|"
+        elsif j == (width-1) && (i!=0) && (i!=height-1)
+          print "|"
+        end
+        # Affiche le haut et le bas du rectangle
+        if i == 0 && (j!=0) && (j!=width-1)
+          print "-"
+        elsif i == (height-1) && (j!=0) && (j!=width-1)
+          print "-"
+        end
+        # Affiche les espaces à l'intérieur du rectangle
+        if (i!=0) && (i!=height-1) && (j!=0) && (j!=width-1)
+          print " "
         end
         j += 1
       end
       print("\n")
       i += 1
     end
-    i += 1
   end
 end
 
-def main()
-  obj = MyRectangle.new()
-
-  obj.draw_rectangle(50, 3)
-end
-main()
-
 # Partie 1: Gestion d'erreur
-# (puts "#{$PROGRAM_NAME} WIDTH LENGTH"; exit) if ARGV.length != 2
+(puts "error arguments : #{$PROGRAM_NAME} HEIGHT WIDTH"; exit) if ARGV.length != 2
 
-# Partie 2 : Parsing
-#length = ARGV[1].to_i
-#width = ARGV[0].to_i
+# Partie 2: Parsing
+def main()
+  width = ARGV[0].to_i
+  height = ARGV[1].to_i
+  obj_rec = MyRectangle.new
+  obj_rec.draw_rectangle(width, height)
+end
 
-# Partie 3: Résolution
-
-
-# Partie 4: Affichage
+# Partie 3: Affichage
+main()
