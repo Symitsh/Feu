247731881bd92bbcd31d33960dd6b9ecde8aeba9
Sandri Florent <sandri822@hotmail.fr>
2022-04-12 07:48:40 +0200

    Revue de code

--- feu00.rb
+++ feu00.rb
@@ -1,32 +1,30 @@
 # ***** Echauffement *****
 class MyRectangle
   # Dessine un rectangle d'une largeur et d'une hauteur données
-  def draw_rectangle(width, height)
+  def draw(width, height)
     i = 0
-    while (i < height)
+    while i < height
       j = 0
-      while (j < width)
+      while j < width
         # Affiche les extrémités du rectangle
-        if (i == (height - 1) && j == (width - 1) || i == (height - 1) && j == 0 ||
-          i == 0 && j == (width - 1) || i == 0 && j == 0)
-          print "o"
+        if i == (height - 1) && j == (width - 1) || i == (height - 1) && j.zero? ||
+          i.zero? && j == (width - 1) || i.zero? && j.zero?
+          print 'o'
         end
         # Affiche le côté gauche et droit du rectangle
-        if j == 0 && (i!=0) && (i!=height-1)
-          print "|"
-        elsif j == (width-1) && (i!=0) && (i!=height-1)
-          print "|"
+        if j.zero? && (i != 0) && (i != height - 1)
+          print '|'
+        elsif j == (width - 1) && (i != 0) && (i != height - 1)
+          print '|'
         end
         # Affiche le haut et le bas du rectangle
-        if i == 0 && (j!=0) && (j!=width-1)
-          print "-"
-        elsif i == (height-1) && (j!=0) && (j!=width-1)
-          print "-"
+        if i.zero? && (j != 0) && (j != width - 1)
+          print '-'
+        elsif i == (height - 1) && (j != 0) && (j != width - 1)
+          print '-'
         end
         # Affiche les espaces à l'intérieur du rectangle
-        if (i!=0) && (i!=height-1) && (j!=0) && (j!=width-1)
-          print " "
-        end
+        print ' ' if (i != 0) && (i != height - 1) && (j != 0) && (j != width - 1)
         j += 1
       end
       print("\n")
@@ -39,12 +37,12 @@ end
 (puts "error arguments : #{$PROGRAM_NAME} HEIGHT WIDTH"; exit) if ARGV.length != 2
 
 # Partie 2: Parsing
-def main()
+def main
   width = ARGV[0].to_i
   height = ARGV[1].to_i
   obj_rec = MyRectangle.new
-  obj_rec.draw_rectangle(width, height)
+  obj_rec.draw(width, height)
 end
 
 # Partie 3: Affichage
-main()
+main
