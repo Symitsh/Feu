c332096a8d00a53160bb78f568046d2641bd5e40
Sandri Florent <sandri822@hotmail.fr>
2022-04-16 20:40:13 +0200

    Ajout méthode evaluate(expression)

--- feu01.rb
+++ feu01.rb
@@ -1,18 +1,9 @@
 # ***** Evaluer une expression *****
 # sans utilisé eval()
 class ArithmeticExpression
-
-  LAMBDAS_FUNCTION = {                          # 2: Utilisation d'un lambda constant pour enregistrer les différents paramètres
-    "+" => lambda {|a, b| a + b},
-    "-" => lambda {|a, b| a - b},
-    "*" => lambda {|a, b| a * b},
-    "/" => lambda {|a, b| a / b},
-    "%" => lambda {|a, b| a % b}
-  }
-
-  def split_expression(expression)              # 1: Méthode qui me permet de séparé l'expression et de la mettre dans un tableau
+  def split_expression(expression)                     # 2: Méthode qui permet de séparer l'expression puis la met dans un tableau
     expression
-    .gsub('(', ' ( ')
+    .gsub('(', ' ( ')                                  # .gsub remplace chaque référence du premier paramètre par le deuxième paramètre
     .gsub(')', ' ) ')
     .gsub('+', ' + ')
     .gsub('-', ' - ')
@@ -22,29 +13,74 @@ class ArithmeticExpression
     .split(' ')
   end
 
-  def operator?(expression)                     # x: Méthode vérifiant un des opérateurs est présent dans la boucle au moment t
-    $priority.include?(expression)
-  end
-
-  def pop_connect_push(array1, array2)
-    temp = array1.pop
-  end
-
-
-  def infix_exp_to_tree(expression)
-    array1, array2 = [], []                     # 3: Je crée 2 tableaux
-    token = split_expression(expression)        # 4: Je stocke ma méthode split_expression dans une variable pour la parcourir avec une boucle each
-
-    token.each do |element|
-      if operator?(element)
-        # Ordonne les priorités
-        until (array1.empty? or
-              array1.last == "(" or
-              $priority[array1.last] < $priority[expression])
-          p pop_connect_push(array1, array2)
-        end
-      end
+  def operator_eval (nb1, signe, nb2)               # Méthode permettant selon le signe de faire un calcul
+    $result = case signe
+      when "+" then nb1 += nb2
+      when "-" then nb1 -= nb2
+      when "*" then nb1 *= nb2
+      when "/" then nb1 /= nb2
+      when "%" then nb1 %= nb2
+      else "Operator error: #{nb1} #{signe} #{nb2} ?" ; exit
     end
+  end
+
+  def include(tokens)                               # Méthode permettant de faire le/les calculs si une parenthèse est inclue dans l'expression
+    array1 = []
+    if tokens.include?("(")
+      opening_parenthesis = tokens.index("(")
+      closing_parenthesis = tokens.index(")")
+      $index_parenthese = opening_parenthesis
+      array1 << tokens[opening_parenthesis..closing_parenthesis]
+      array1.flatten!
+      tokens -= array1
+      array1.shift ; array1.pop
+      tokens.insert($index_parenthese, evaluate(array1))
+      tokens.flatten!
+      evaluate(tokens)
+    else
+      evaluate(tokens)
+    end
+  end
+
+  def method_name(expression)                          # Méthode permettant de faire un lien entre les méthodes
+    tokens = split_expression(expression)
+    include(tokens)
+  end
+
+  def evaluate(expression)                              # Evalue les priorités des signes
+    tab3 = []
+    expression.each do |element|
+      if element == "/" || element == "*" || element == "%"
+        index_signe = expression.index(element)
+        index_nb1 = index_signe - 1
+        index_nb2 = index_signe + 1
+        tab3.clear
+        tab3 << expression[index_nb1..index_nb2]
+        tab3.flatten!
+        expression -= tab3
+        nb1 = tab3[0].to_i ; signe = tab3[1] ; nb2 = tab3[2].to_i
+        operator_eval(nb1, signe, nb2)
+        expression.insert(index_nb1, $result.to_s)
+      end
+      (expression.insert(index_signe, signe)) if expression.length == 2
+    end
+
+    expression.each do |element|
+      if element == "+" || element == "-"
+        index_signe = expression.index(element)
+        index_nb1 = index_signe - 1
+        index_nb2 = index_signe + 1
+        tab3.clear
+        tab3 << expression[index_nb1..index_nb2]
+        tab3.flatten!
+        expression -= tab3
+        nb1 = tab3[0].to_i ; signe = tab3[1] ; nb2 = tab3[2].to_i
+        operator_eval(nb1, signe, nb2)
+        expression.insert(index_nb1, $result.to_s)
+      end
+      (expression.insert(index_signe, signe)) if expression.length == 2
+    end
+    return expression
   end
 end
 
@@ -55,12 +91,9 @@ end
 def main
   expression = ARGV[0]
   obj = ArithmeticExpression.new
-  p obj.split_expression(expression)
-  obj.infix_exp_to_tree(expression)
+  obj.split_expression(expression)
+  obj.method_name(expression)
 end
-$priority = {"+" => 0, "-" => 0, "*" => 1, "/" => 1, "%" => 1}
 
 # Partie 3: Affichage
-main
-
-# [\+\-\/\*\%]
+puts main
