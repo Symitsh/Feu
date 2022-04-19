e977e67fed1bac92bcca34e7c136b45943f88b3d
Sandri Florent <sandri822@hotmail.fr>
2022-04-19 21:27:10 +0200

    Ajout méthode print_sudoku(sudoku)

--- feu03.rb
+++ feu03.rb
@@ -1,16 +1,4 @@
 # ***** Sudoku *****
-=begin
-1. Sur chaque ligne, dès qu'on trouve un '.' on doit mettre un nombre compris entre 1..9
-2. Ce nombre X à 3 contrainte à respecté
-_ Il doit apparrait 1 fois par ligne
-_ Il dois apparait 1 fois par colonne
-_ Il doit apparait 1 fois par carré
-SI c'est BON, je fait une boucle de 1. et 2.
-Dès que je trouve un point je met un nombre et je vérifie que toute les contraintes soit bonne
-
-3. Arrivé à la Fin du tableau et ayant remplie tout les points.
-=end
-
 class Sudoku
   # méthodes utilisées
   def display_sudoku()
@@ -29,7 +17,29 @@ class Sudoku
 
   end
 
-  def check_sudoku(sudoku)
+  def print_sudoku(sudoku)
+    i = 0
+    while (i < sudoku.count)
+      j = 0
+      while (j < sudoku[i].count)
+        print sudoku[i][j]
+        j += 1
+      end
+      print "\n"
+      i += 1
+    end
+  end
+
+
+  def check_sudoku
+    file = ARGV[0]
+    sudoku = []
+    position = 0
+    File.foreach(file) do |line|
+      sudoku[position] = line.chomp.split('')
+      position += 1
+    end
+    sudoku = print_sudoku(sudoku)
 
   end
 end
@@ -42,14 +52,28 @@ unless ARGV.size == 1
 end
 
 # Partie 2: Parsing
-s = ARGV[0]
-sudoku = []
-position = 0
+
 
 def main
   obj = Sudoku.new
-  obj.check_sudoku(sudoku)
+  obj.check_sudoku
 end
 
 # Partie 3: Affichage
 puts main
+
+=begin
+
+1. Sur chaque ligne, dès qu'il y a trouve un '.' affiche un nombre compris entre 1..9
+
+2. Ce nombre X à 3 contraintes à respecté
+_ Il doit apparaître 1 fois par ligne
+_ Il doit apparaître 1 fois par colonne
+_ Il doit apparaître 1 fois par carré
+
+4. SI BON, fait une boucle de la partie 1. et 2.
+Dès qu'il y a un point met un nombre et vérifie que toutes les contraintes soient bonnes
+
+5. Arrivé à la Fin du tableau en ayant rempli tous les points.
+
+=end
