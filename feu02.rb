e5a316d316c95235c2eec4fb2c5bd04e4bb6cc01
Sandri Florent <sandri822@hotmail.fr>
2022-04-19 12:20:15 +0200

    Construction de la méthode gives_position(file1, file2)

--- feu02.rb
+++ feu02.rb
@@ -4,35 +4,41 @@ class FindShape
   def reading_data(file)
     i = 0
     res = []
-    File.foreach(file) do |line|
-      res [i] = []
-      res[i] += line.chomp.split('')
+    File.foreach(file) do |element|
+      res[i] = []
+      res[i] += element.chomp.split('')
       i += 1
     end
     res
   end
 
   def gives_position(file1, file2)
-    (0..(file2.size - 1)).each do |i|
-      (0..(file2[i].size - 1)).each do |j|
-        next unless file2[i][j] == file1[0][0]
-
+    (0..(file1.size - 1)).each do |line|
+      (0..(file1[line].size - 1)).each do |column|
+        if file2[0][0] == file1[line][column] &&
+           file2[0][1] == file1[line][column + 1] &&
+           file2[1][1] == file1[line + 1][column + 1]
+          return "#{column}, #{line}"
+        end
       end
     end
+    puts 'Introuvable'
+    exit
   end
 end
 
 # Partie 1: Gestion d'erreur
 unless ARGV.size == 2
-  puts "** error _#{__FILE__}_ **\nPass two data files.\nThe first is the shape to search for."
+  puts "*** error ***\n _#{__FILE__}_\nPass two data files.\n
+  The first is the shape to search for."
   exit
 end
 
 # Partie 2: Parsing
 def main
   obj = FindShape.new
-  file1 = obj.reading_data(ARGV[0])
-  file2 = obj.reading_data(ARGV[1])
+  file1 = obj.reading_data(ARGV[0]) # board
+  file2 = obj.reading_data(ARGV[1]) # to_find
   obj.gives_position(file1, file2)
 end
 
