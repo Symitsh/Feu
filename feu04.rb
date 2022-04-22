768111dd5566b7fb9a400fd632e20655558a3774
Sandri Florent <sandri822@hotmail.fr>
2022-04-22 18:21:38 +0200

    Modification de la structure

--- feu04.rb
+++ feu04.rb
@@ -9,45 +9,85 @@ class MaximalSquare
     end
   end
 
-  def maximum_square(plateau) #  Trouve la taille manimal avec des o
-    row = plateau.length
-    col = plateau[0].length
-    #  Stock un plateau de '.' dans une variable
-    auxiliary = Array.new(row) { Array.new(col) { '.' } }
-
+  def print_matrix(matrix)
+    i = 0
+    while i < matrix.count
+      j = 0
+      while j < matrix[i].count
+        print matrix[i][j]
+        j += 1
+      end
+      print "\n"
+      i += 1
+    end
   end
 
-  def get_top_left_location() # localise le haut gauche du carré
-
+  def init_array
+    new_array = []
+    value = 1
+    while value < 10
+      new_array[value] = 0
+      value += 1
+    end
+    new_array
   end
 
-  def display_result() # Affiche le résultat
+  def solve_biggest_square(matrix)
+    i = 0
+    while i < matrix.count
+      j = 0
+      while j < matrix[i].count
+        if matrix[i][j] == '.'
 
-    print("\n")
+        end
+        j += 1
+      end
+      i += 1
+    end
+    matrix
+  end
+
+  def check_plateau
+    file = ARGV[0]
+    matrix = []
+    position = 0
+
+    File.foreach(file) do |line|
+      matrix[position] = line.chomp.split('')
+      position += 1
+      # puts "\n"
+    end
+    matrix = solve_biggest_square(matrix)
+    if matrix == false
+      puts 'error'
+    else
+      matrix = print_matrix(matrix)
+    end
   end
 end
 
+# Partie 1: Gestion d'erreur
+unless ARGV.size == 1
+  puts "*** error ***\n _#{__FILE__}_
+  Only one data file."
+  exit
+end
+
+# Partie 2: Parsing
 def main # Teste avec un plateau fixe prédéfinie
   obj = MaximalSquare.new
-  plateau = [
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ]
-  obj.maximum_square(plateau)
+  obj.check_plateau
 end
 
-main
+# Partie 3: Affichage
+puts main
 
+#  TODO Lance automatiquement avec commande
 #
-# Générateur de plateau
-# def main()
+# Générateur de plateau qui envoie dans un fichier texte
+#
+# def plateau_generator
+#   File.delete("plateau.txt")
 #   if ARGV.count != 3
 #     puts "params needed: x y density"
 #     exit
@@ -57,12 +97,13 @@ main
 #   y = ARGV[1].to_i
 #   density = ARGV[2].to_i
 #
-#   puts "#{y}.xo"
+#   # open('plateau.txt', 'a') {|f| f << "#{y}"}
+#   # open('plateau.txt', 'a') {|f| f << "\n"}
 #   for i in 0..y do
 #     for j in 0..x do
-#         print ((rand(y) * 2 < density) ? 'x' : '.')
+#       open('plateau.txt', 'a') {|f| f << ((rand(y) * 2 < density) ? 'o' : '.')}
 #     end
-#     print "\n"
+#     open('plateau.txt', 'a') {|f| f << "\n"}
 #   end
 # end
 #
