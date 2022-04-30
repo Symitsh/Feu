d2fbd707a3f46913668146c428af1770f2a6c1d0
Sandri Florent <sandri822@hotmail.fr>
2022-04-30 17:00:29 +0200

    Indispensable pour feu05.rb

---
+++ labyrinth_generator.rb
@@ -0,0 +1,29 @@
+# Programme permettant de généré un labyrinth avec en paramètre :
+# la largeur, hauteur, un char(chemin), les portes(entrée, sortie)
+# Exemple: `ruby labyrinth_generator.rb 20 20 "* o12"`
+
+def labyrinth_generator
+  File.delete("labyrinth.txt")
+  height, width, chars, gates = ARGV[0].to_i, ARGV[1].to_i, ARGV[2], ARGV[3].to_i
+  entry = rand(width - 4) + 2
+  entry2 = rand(width - 4) + 2
+
+  open('labyrinth.txt', 'a') {|f| f << "#{height}x#{width}#{ARGV[2]}"}
+  open('labyrinth.txt', 'a') {|f| f << "\n"}
+  height.times do |y|
+    width.times do |x|
+      if y == 0 && x == entry
+        open('labyrinth.txt', 'a') {|f| f << chars[4].chr}
+      elsif y == height - 1 && x == entry2
+        open('labyrinth.txt', 'a') {|f| f << chars[3].chr}
+      elsif y.between?(1, height - 2) && x.between?(1, width - 2) && rand(100) > 15
+        open('labyrinth.txt', 'a') {|f| f << chars[1].chr}
+      else
+        open('labyrinth.txt', 'a') {|f| f << chars[0].chr}
+      end
+    end
+    open('labyrinth.txt', 'a') {|f| f << "\n"}
+  end
+end
+
+labyrinth_generator
